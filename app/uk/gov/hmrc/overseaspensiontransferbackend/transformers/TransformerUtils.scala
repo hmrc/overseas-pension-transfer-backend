22e475f65757e5088811556af1267329616252ad
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-07-07 14:35:25 +0100

    OAOTC-1082 Add flattenName helper method

--- app/uk/gov/hmrc/overseaspensiontransferbackend/transformers/TransformerUtils.scala
+++ app/uk/gov/hmrc/overseaspensiontransferbackend/transformers/TransformerUtils.scala
@@ -17,8 +17,77 @@
 package uk.gov.hmrc.overseaspensiontransferbackend.transformers
 
 import play.api.libs.json._
+import uk.gov.hmrc.overseaspensiontransferbackend.utils.JsonHelpers
 
 object TransformerUtils {
+
+  /** Flattens a nested JSON representation of a person's name into a flat structure.
+    *
+    * This is designed to work with data structures like: { "name": { "firstName": "Jane", "lastName": "Doe" } } which will be flattened to: { "foreName":
+    * "Jane", "lastName": "Doe" }
+    */
+  def flattenName(
+      path: JsPath,
+      foreNameKey: String = "foreName",
+      lastNameKey: String = "lastName"
+    )(
+      json: JsObject
+    ): Either[JsError, JsObject] = {
+    path.asSingleJson(json).asOpt[JsObject] match {
+      case Some(nameObj) =>
+        val firstNameOpt = (nameObj \ "firstName").asOpt[String]
+        val lastNameOpt  = (nameObj \ "lastName").asOpt[String]
+
+        val nameFields = Seq(
+          firstNameOpt.map(foreNameKey -> JsString(_)),
+          lastNameOpt.map(lastNameKey -> JsString(_))
+        ).flatten
+
+        val nameObject = JsObject(nameFields)
+
+        val parentPath = JsPath(path.path.dropRight(1))
+
+        JsonHelpers.setPath(parentPath, nameObject, json)
+
+      case None =>
+        Right(json)
+    }
+  }
+
+  /** Unflattens a nested JSON representation of a person's name into a flat structure.
+    */
+  def unflattenName(
+                     foreNameKey: String = "foreName",
+                     lastNameKey: String = "lastName",
+                     path: JsPath
+                   ): JsObject => Either[JsError, JsObject] = { json =>
+    val parentPath = JsPath(path.path.dropRight(1))
+    val parentOpt  = parentPath.asSingleJson(json).asOpt[JsObject]
+
+    parentOpt match {
+      case Some(parent) =>
+        val foreNameOpt = (parent \ foreNameKey).asOpt[String]
+        val lastNameOpt = (parent \ lastNameKey).asOpt[String]
+
+        if (foreNameOpt.isEmpty && lastNameOpt.isEmpty) {
+          Right(json)
+        } else {
+          val nameFields = Seq(
+            foreNameOpt.map("firstName" -> JsString(_)),
+            lastNameOpt.map("lastName" -> JsString(_))
+          ).flatten
+          val nameObject = JsObject(nameFields)
+          val updatedParent = parent - foreNameKey - lastNameKey
+          val withName = updatedParent + (path.path.last.asInstanceOf[KeyPathNode].key -> nameObject)
+
+         JsonHelpers.setPath(parentPath, withName, json)
+        }
+
+      case None =>
+        Right(json)
+    }
+  }
+
 
   def applyPipeline[T](
       json: JsObject,
---
+++ test/uk/gov/hmrc/overseaspensiontransferbackend/transformers/TransformerUtilsSpec.scala
@@ -0,0 +1,138 @@
+package uk.gov.hmrc.overseaspensiontransferbackend.transformers
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json._
+
+class TransformerUtilsSpec extends AnyFreeSpec with Matchers {
+
+  "flattenName" - {
+    "should flatten name with firstName and lastName to foreName and lastName" in {
+      val input = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj(
+            "firstName" -> "Alice",
+            "lastName"  -> "Smith"
+          )
+        )
+      )
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj(
+          "foreName" -> "Alice",
+          "lastName" -> "Smith"
+        )
+      )
+
+      val result = TransformerUtils.flattenName(__ \ "memberDetails" \ "name")(input)
+      result mustBe Right(expected)
+    }
+
+    "should flatten name with only firstName" in {
+      val input = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj("firstName" -> "Alice")
+        )
+      )
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj("foreName" -> "Alice")
+      )
+
+      TransformerUtils.flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should flatten name with only lastName" in {
+      val input = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj("lastName" -> "Smith")
+        )
+      )
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj("lastName" -> "Smith")
+      )
+
+      TransformerUtils.flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should return original JSON if path is missing" in {
+      val input = Json.obj("memberDetails" -> Json.obj("nino" -> "AB123456C"))
+
+      TransformerUtils.flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(input)
+    }
+
+    "should use custom keys if provided" in {
+      val input = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")
+        )
+      )
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj(
+          "given" -> "Alice",
+          "surname" -> "Smith"
+        )
+      )
+
+      TransformerUtils.flattenName(__ \ "memberDetails" \ "name", foreNameKey = "given", lastNameKey = "surname")(input) mustBe Right(expected)
+    }
+  }
+
+  "unflattenName" - {
+    "should reconstruct name field from foreName and lastName" in {
+      val input = Json.obj(
+        "memberDetails" -> Json.obj(
+          "foreName" -> "Alice",
+          "lastName" -> "Smith"
+        )
+      )
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj(
+            "firstName" -> "Alice",
+            "lastName"  -> "Smith"
+          )
+        )
+      )
+
+      TransformerUtils.unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should return original JSON if neither name part exists" in {
+      val input = Json.obj("memberDetails" -> Json.obj("nino" -> "AB123456C"))
+
+      TransformerUtils.unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(input)
+    }
+
+    "should reconstruct name from only foreName" in {
+      val input = Json.obj("memberDetails" -> Json.obj("foreName" -> "Alice"))
+
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice")))
+
+      TransformerUtils.unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should reconstruct name from only lastName" in {
+      val input = Json.obj("memberDetails" -> Json.obj("lastName" -> "Smith"))
+
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("lastName" -> "Smith")))
+
+      TransformerUtils.unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should support custom keys" in {
+      val input = Json.obj("memberDetails" -> Json.obj("given" -> "Alice", "surname" -> "Smith"))
+
+      val expected = Json.obj(
+        "memberDetails" -> Json.obj(
+          "name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")
+        )
+      )
+
+      TransformerUtils.unflattenName(foreNameKey = "given", lastNameKey = "surname", path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+  }
+}
