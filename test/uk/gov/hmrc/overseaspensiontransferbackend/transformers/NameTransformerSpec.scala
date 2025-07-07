728f0ccd29b72ffe96f12cc813fc1b158612224b
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-07-07 17:18:36 +0100

    OAOTC-1082 Update specs to reflect refactor of transformer utils

---
+++ test/uk/gov/hmrc/overseaspensiontransferbackend/transformers/NameTransformerSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.overseaspensiontransferbackend.transformers
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json._
+
+class NameTransformerSpec extends AnyFreeSpec with Matchers with NameTransformers {
+
+  "flattenName" - {
+    "should flatten name with firstName and lastName to foreName and lastName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")))
+      val expected = Json.obj("memberDetails" -> Json.obj("foreName" -> "Alice", "lastName" -> "Smith"))
+
+      flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should flatten name with only firstName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice")))
+      val expected = Json.obj("memberDetails" -> Json.obj("foreName" -> "Alice"))
+
+      flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should flatten name with only lastName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("lastName" -> "Smith")))
+      val expected = Json.obj("memberDetails" -> Json.obj("lastName" -> "Smith"))
+
+      flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should return original JSON if path is missing" in {
+      val input = Json.obj("memberDetails" -> Json.obj("nino" -> "AB123456C"))
+      flattenName(__ \ "memberDetails" \ "name")(input) mustBe Right(input)
+    }
+
+    "should use custom keys if provided" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")))
+      val expected = Json.obj("memberDetails" -> Json.obj("given" -> "Alice", "surname" -> "Smith"))
+
+      flattenName(__ \ "memberDetails" \ "name", foreNameKey = "given", lastNameKey = "surname")(input) mustBe Right(expected)
+    }
+  }
+
+  "unflattenName" - {
+    "should reconstruct name field from foreName and lastName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("foreName" -> "Alice", "lastName" -> "Smith"))
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")))
+
+      unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should return original JSON if neither name part exists" in {
+      val input = Json.obj("memberDetails" -> Json.obj("nino" -> "AB123456C"))
+      unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(input)
+    }
+
+    "should reconstruct name from only foreName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("foreName" -> "Alice"))
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice")))
+
+      unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should reconstruct name from only lastName" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("lastName" -> "Smith"))
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("lastName" -> "Smith")))
+
+      unflattenName(path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+
+    "should support custom keys" in {
+      val input    = Json.obj("memberDetails" -> Json.obj("given" -> "Alice", "surname" -> "Smith"))
+      val expected = Json.obj("memberDetails" -> Json.obj("name" -> Json.obj("firstName" -> "Alice", "lastName" -> "Smith")))
+
+      unflattenName(foreNameKey = "given", lastNameKey = "surname", path = __ \ "memberDetails" \ "name")(input) mustBe Right(expected)
+    }
+  }
+}
--- test/uk/gov/hmrc/overseaspensiontransferbackend/utils/JsonHelpersSpec.scala
+++ test/uk/gov/hmrc/overseaspensiontransferbackend/utils/JsonHelpersSpec.scala
@@ -20,55 +20,55 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import play.api.libs.json._
 
-class JsonHelpersSpec extends AnyFreeSpec with Matchers {
+class JsonHelpersSpec extends AnyFreeSpec with Matchers with JsonHelpers {
 
   "JsonHelpers" - {
 
     "movePath should move a value from one path to another" in {
       val input  = Json.obj("a" -> Json.obj("b" -> "value"))
-      val result = JsonHelpers.movePath(__ \ "a" \ "b", __ \ "x" \ "y", input)
+      val result = movePath(__ \ "a" \ "b", __ \ "x" \ "y", input)
       // "a" gets removed, see prunePath comment
       result mustBe Right(Json.obj("x" -> Json.obj("y" -> JsString("value"))))
     }
 
     "movePath should return unchanged JSON if source path does not exist" in {
       val input  = Json.obj("a" -> Json.obj("b" -> "value"))
-      val result = JsonHelpers.movePath(__ \ "a" \ "missing", __ \ "x" \ "y", input)
+      val result = movePath(__ \ "a" \ "missing", __ \ "x" \ "y", input)
 
       result mustBe Right(input)
     }
 
     "setPath should set a value at a nested path" in {
       val input  = Json.obj("a" -> Json.obj())
-      val result = JsonHelpers.setPath(__ \ "a" \ "b" \ "c", JsString("value"), input)
+      val result = setPath(__ \ "a" \ "b" \ "c", JsString("value"), input)
 
       result mustBe Right(Json.obj("a" -> Json.obj("b" -> Json.obj("c" -> "value"))))
     }
 
     "setPath should overwrite existing value at path" in {
       val input  = Json.obj("a" -> Json.obj("b" -> Json.obj("c" -> "old")))
-      val result = JsonHelpers.setPath(__ \ "a" \ "b" \ "c", JsString("new"), input)
+      val result = setPath(__ \ "a" \ "b" \ "c", JsString("new"), input)
 
       result mustBe Right(Json.obj("a" -> Json.obj("b" -> Json.obj("c" -> "new"))))
     }
 
     "setPath should fail for unsupported paths" in {
       val input  = Json.obj("a" -> Json.obj())
-      val result = JsonHelpers.setPath(JsPath(Nil), JsString("value"), input)
+      val result = setPath(JsPath(Nil), JsString("value"), input)
 
       result mustBe Right(input) // This is acceptable per your implementation (no-op on empty path)
     }
 
     "prunePath should remove a nested key" in {
       val input  = Json.obj("a" -> Json.obj("b" -> "value", "c" -> "keep"))
-      val result = JsonHelpers.movePath(__ \ "a" \ "b", __ \ "x", input)
+      val result = movePath(__ \ "a" \ "b", __ \ "x", input)
 
       result mustBe Right(Json.obj("a" -> Json.obj("c" -> "keep"), "x" -> "value"))
     }
 
     "setPath should create intermediate objects if necessary" in {
       val input  = Json.obj()
-      val result = JsonHelpers.setPath(__ \ "a" \ "b" \ "c", JsString("new"), input)
+      val result = setPath(__ \ "a" \ "b" \ "c", JsString("new"), input)
 
       result mustBe Right(Json.obj("a" -> Json.obj("b" -> Json.obj("c" -> "new"))))
     }
