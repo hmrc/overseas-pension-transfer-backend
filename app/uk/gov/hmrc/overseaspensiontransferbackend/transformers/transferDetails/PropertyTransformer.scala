f6e66f327af075ffbd79173834f0b8677620fbfa
Tateni93 <33066342+Tateni93@users.noreply.github.com>
2025-07-30 16:31:56 +0100

    OAOTC-1315: Added PropertyTransformer

---
+++ app/uk/gov/hmrc/overseaspensiontransferbackend/transformers/transferDetails/PropertyTransformer.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.overseaspensiontransferbackend.transformers.transferDetails
+
+import play.api.libs.json.{JsError, JsObject, JsPath}
+import uk.gov.hmrc.overseaspensiontransferbackend.transformers.{PathAwareTransformer, TransformerUtils}
+import uk.gov.hmrc.overseaspensiontransferbackend.transformers.steps.{moveStep, TransformerStep}
+import uk.gov.hmrc.overseaspensiontransferbackend.transformers.transformerSteps.AddressTransformerStep
+
+class PropertyTransformer extends PathAwareTransformer with AddressTransformerStep {
+
+  override def externalPath: JsPath = JsPath \ "transferDetails" \ "propertyAssets"
+
+  override def internalPath: JsPath = JsPath \ "transferDetails" \ "typeOfAssets" \ "propertyAssets"
+
+  /** Applies a transformation from raw frontend input (e.g. UserAnswersDTO.data) into the correct internal shape for AnswersData.
+    */
+  override def construct(input: JsObject): Either[JsError, JsObject] = {
+    val steps: Seq[TransformerStep] = Seq(
+      moveStep(externalPath, internalPath)
+    )
+
+    TransformerUtils.applyPipeline(input, steps)(identity)
+  }
+
+  /** Applies the reverse transformation to make stored data suitable for frontend rendering.
+    */
+  override def deconstruct(input: JsObject): Either[JsError, JsObject] = {
+    val steps: Seq[TransformerStep] = Seq(
+      moveStep(internalPath, externalPath)
+    )
+
+    TransformerUtils.applyPipeline(input, steps)(identity)
+  }
+}
