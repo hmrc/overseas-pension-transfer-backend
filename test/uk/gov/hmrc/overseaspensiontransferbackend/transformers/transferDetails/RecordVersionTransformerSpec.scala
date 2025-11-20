744810ee757f5edf30c4e31757826615422dac41
Tateni93 <33066342+Tateni93@users.noreply.github.com>
2025-11-20 15:30:09 +0000

    OAOTC-1718: Added TypeOfAssets RecordVersion transformer to avoid value being pruned

---
+++ test/uk/gov/hmrc/overseaspensiontransferbackend/transformers/transferDetails/RecordVersionTransformerSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.overseaspensiontransferbackend.transformers.transferDetails
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json.Json
+
+class RecordVersionTransformerSpec extends AnyFreeSpec with Matchers {
+
+  private val transformer = new RecordVersionTransformer
+
+  "QropsNameTransformer" - {
+
+    "must move transferDetails.typeOfAssets.recordVersion to transferDetails.typeOfAssetsVersion.recordVersion" in {
+      val inputJson = Json.obj("transferDetails" -> Json.obj("typeOfAssetsVersion" -> Json.obj("recordVersion" -> "001")))
+      val expected  = Json.obj("transferDetails" -> Json.obj("typeOfAssets" -> Json.obj("recordVersion" -> "001")))
+
+      transformer.construct(inputJson) mustBe Right(expected)
+    }
+
+    "must move transferDetails.typeOfAssetsVersion.recordVersion to transferDetails.typeOfAssets.recordVersion" in {
+      val inputJson = Json.obj("transferDetails" -> Json.obj("typeOfAssets" -> Json.obj("recordVersion" -> "001")))
+      val expected  = Json.obj("transferDetails" -> Json.obj("typeOfAssetsVersion" -> Json.obj("recordVersion" -> "001")))
+
+      transformer.deconstruct(inputJson) mustBe Right(expected)
+    }
+
+    "must leave JSON unchanged if transferDetails.typeOfAssets.recordVersion is missing" in {
+      val inputJson = Json.obj("transferDetails" -> Json.obj("typeOfAssets" -> Json.obj()))
+
+      transformer.construct(inputJson) mustBe Right(inputJson)
+    }
+
+    "must leave JSON unchanged if transferDetails.typeOfAssetsVersion.recordVersion is missing" in {
+      val inputJson = Json.obj("transferDetails" -> Json.obj("typeOfAssetsVersion" -> Json.obj()))
+
+      transformer.deconstruct(inputJson) mustBe Right(inputJson)
+    }
+  }
+}
